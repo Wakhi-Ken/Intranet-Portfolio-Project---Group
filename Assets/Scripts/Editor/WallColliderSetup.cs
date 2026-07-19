2fd1dc50b4b0b1c7f652aa99e4a5cb9c3b160b05
audreym101 <m.audrey1@alustudent.com>
2026-07-20 02:07:17 +0300

    Fix wall collider tool to target correct maze object and repair mismatched collider meshes

--- Assets/Scripts/Editor/WallColliderSetup.cs
+++ Assets/Scripts/Editor/WallColliderSetup.cs
@@ -6,27 +6,35 @@ public class WallColliderSetup
     [MenuItem("Tools/Add Wall Colliders to Maze")]
     static void AddWallColliders()
     {
-        GameObject maze = GameObject.Find("maze");
+        GameObject maze = GameObject.Find("walls");
         if (maze == null)
         {
-            Debug.LogError("Could not find GameObject named 'maze'. Make sure it exists in the scene.");
+            Debug.LogError("Could not find GameObject named 'walls'. Make sure it exists in the scene.");
             return;
         }
 
         MeshFilter[] meshFilters = maze.GetComponentsInChildren<MeshFilter>(true);
         int added = 0;
+        int fixedMesh = 0;
 
         foreach (MeshFilter mf in meshFilters)
         {
-            if (mf.GetComponent<Collider>() != null) continue;
+            MeshCollider mc = mf.GetComponent<MeshCollider>();
+            if (mc == null)
+            {
+                mc = mf.gameObject.AddComponent<MeshCollider>();
+                mc.convex = false;
+                added++;
+            }
 
-            MeshCollider mc = mf.gameObject.AddComponent<MeshCollider>();
-            mc.sharedMesh = mf.sharedMesh;
-            mc.convex = false;
-            added++;
+            if (mc.sharedMesh != mf.sharedMesh)
+            {
+                mc.sharedMesh = mf.sharedMesh;
+                fixedMesh++;
+            }
         }
 
-        Debug.Log($"Added {added} MeshColliders to maze walls.");
+        Debug.Log($"Added {added} MeshColliders and fixed {fixedMesh} mismatched mesh references on maze walls.");
         EditorUtility.SetDirty(maze);
     }
 }
